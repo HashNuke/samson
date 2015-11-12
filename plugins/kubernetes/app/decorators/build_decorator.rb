5e051c1a10630ab41502aef0d578db97595431d1
Sergio Nunes <snunes@zendesk.com>
2015-11-12 16:02:08 +0000

    Import API read-only for now.

--- plugins/kubernetes/app/decorators/build_decorator.rb
+++ plugins/kubernetes/app/decorators/build_decorator.rb
@@ -7,9 +7,6 @@ Build.class_eval do
   end
 
   def import_kubernetes_roles
-    # TODO : Build -> KubernetesRoles association
-    project.roles.destroy_all
-
     config_files = project.repository.directory_contents(git_sha, path: 'kubernetes', ).select { |file|
       file.ends_with?('.yml', '.yaml', '.json')
     }
@@ -18,8 +15,7 @@ Build.class_eval do
       file_contents = file_from_repo(file)
       config_file = Kubernetes::RoleConfigFile.new(file_contents, file)
 
-      # TODO : Build -> KubernetesRoles association
-      role = project.roles.build(
+      Kubernetes::Role.new(
         project: project,
         name: config_file.replication_controller.labels[:role],
         config_file: file,
@@ -28,8 +24,6 @@ Build.class_eval do
         cpu: config_file.replication_controller.pod_template.container.cpu,
         replicas: config_file.replication_controller.replicas,
         deploy_strategy: config_file.replication_controller.deploy_strategy)
-      role.save!
-      role
     end
   end
 end
